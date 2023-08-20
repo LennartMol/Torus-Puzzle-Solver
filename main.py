e04e47c0227cf6ab9f45e22089f3950c0342179b
Lennart Mol <lc.mol@student.avans.nl>
2023-08-20 23:49:37 +0200

    Move methods for rows and columns

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import random as random
 
 # generate 6x6 matrix
-row1 = ["01", "02", "03", "04", "05", "06"]
-row2 = ["07", "08", "09", "10", "11", "12"]
-row3 = ["13", "14", "15", "16", "17", "18"]
-row4 = ["19", "20", "21", "22", "23", "24"]
-row5 = ["25", "26", "27", "28", "29", "30"]
-row6 = ["31", "32", "33", "34", "35", "36"]
-matrix = [row1, row2, row3, row4, row5, row6]
+column1 = ["01", "02", "03", "04", "05", "06"]
+column2 = ["07", "08", "09", "10", "11", "12"]
+column3 = ["13", "14", "15", "16", "17", "18"]
+column4 = ["19", "20", "21", "22", "23", "24"]
+column5 = ["25", "26", "27", "28", "29", "30"]
+column6 = ["31", "32", "33", "34", "35", "36"]
+matrix = [column1, column2, column3, column4, column5, column6]
+
+def moveRowUp(row, moves):
+    for noMoves in range(0, moves):
+        temp = matrix[0][row]
+        for i in range(0, len(matrix)-1):
+            matrix[i][row] = matrix[i+1][row]
+        matrix[len(matrix)-1][row] = temp
+
+def moveColumnLeft(column, moves):
+    for noMoves in range(0, moves):
+        temp = matrix[column][0]
+        for i in range(0, len(matrix)-1):
+            matrix[column][i] = matrix[column][i+1]
+        matrix[column][len(matrix)-1] = temp
+
 
 def randomizeMatrix():
     # randomize columns
@@ -15,14 +30,12 @@ def randomizeMatrix():
         matrix[i] = random.sample(matrix[i], len(matrix[i]))
 
     # randomize rows
-    for column in range(0, len(matrix)):
+    for row in range(0, len(matrix)):
         # move a rows down by random amount (1-5)
         moveDowns = random.randint(1, len(matrix)-1)
-        for moves in range(0, moveDowns):
-            temp = matrix[0][column]
-            for i in range(0, len(matrix)-1):
-                matrix[i][column] = matrix[i+1][column]
-            matrix[len(matrix)-1][column] = temp
+        moveRowUp(row, moveDowns)
+        
+            
 
 randomizeMatrix()
 
@@ -33,8 +46,9 @@ def printMatrix():
 
 printMatrix()
 
+
 # find 2 and move to right spot
 for i in range(0, len(matrix)):
     for j in range(0, len(matrix[i])):
         if matrix[i][j] == "02":
-            print("2 found at: ", i, j)
+            print("2 found at: ", i+1, j+1)
